3d9eca6329583f423fcd4826445030aabf24c604
Mihail Costea <cmihail@chromium.org>
2014-08-26 10:19:46 +0900

    Fix *.sh files headers.
    Fix *.sh files headers and modify Makefile in order to allow custom
    build for debug executables. This is needed as "#/bin/bash -e" will kill
    the bash script in case of any errors, which was the case with make debug
    and index.html.
    
    BUG=chromium:407007
    Change-Id: I15b3b99ebb834f655675548eaa879e94adb43fa9

--- unrar-test/run_js_tests.sh
+++ unrar-test/run_js_tests.sh
@@ -1,5 +1,10 @@
-#!/bin/bash
+#!/bin/bash -e
+
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 cd ../unrar/
-make debug
+make debug_for_tests
 cd ../unrar-test/
 karma start
--- unrar-test/run_nacl_tests.sh
+++ unrar-test/run_nacl_tests.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+#!/bin/bash -e
+
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 cd cpp
 make  # Catch compile errors.
 make run 2> /dev/null  # Ignore any output except for tests.
